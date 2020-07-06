0f88ff6679b7af1ead3b1d7f93be8016b6bacaab
Mateus <moraes1997mateus@hotmail.com>
2020-07-06 19:36:11 -0300

    Improve bag-of-words cicle

--- natural-language-processing/bag-of-words/fake.py
+++ natural-language-processing/bag-of-words/fake.py
@@ -5,10 +5,13 @@ import numpy as np
 import heapq 
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.probability import FreqDist
+from nltk.corpus import stopwords
+from nltk import ngrams
 
 #TODO implementar função para ser chamada pela nossa API para Tokenization
 text = "Hello word"
 text2 = open("teste.txt", "r")
+text3 = open("teste2.txt", "r")
 
 '''
 First, we will convert text to lower case, then remove all non-word characters 
@@ -29,36 +32,56 @@ to words, and for each word in sentence, we check if the word exists in our
 dictionary. If it does, then we increment its count by 1. If it doesn’t, we add 
 it to our dictionary and set its count as 1.
 '''
+
+def word_extraction(sentence):
+    ignore = set(stopwords.words('portuguese'))
+    cleaned_text = [w.lower() for w in words if w not in ignore]
+    return cleaned_text
+
+ignore = set(stopwords.words('portuguese'))
 word2count = {}
 for data in dataset:
     words = nltk.word_tokenize(data)
     for word in words:
-        if word not in word2count.keys():
-            word2count[word] = 1
-        else:
-            word2count[word] += 1
+        if word not in ignore:
+            if word not in word2count.keys():
+                word2count[word] = 1
+            else:
+                word2count[word] += 1
 
 freq_words = heapq.nlargest(100, word2count, key=word2count.get)
 
-# print("Palavras frequentes", freq_words)
+print("Palavras frequentes", freq_words)
 
 '''
 In this step we construct a vector, which would tell us whether a word in each 
 sentence is a frequent word or not. If a word in a sentence is a frequent word, 
 we set it as 1, else we set it as 0.
 '''
-X = [] 
-for data in dataset: 
-    vector = [] 
-    for word in freq_words: 
-        if word in nltk.word_tokenize(data): 
-            vector.append(1) 
-        else: 
-            vector.append(0) 
-    X.append(vector) 
-X = np.asarray(X)
+# X = [] 
+# for data in dataset: 
+#     vector = [] 
+#     for word in freq_words: 
+#         if word in nltk.word_tokenize(data): 
+#             vector.append(1) 
+#         else: 
+#             vector.append(0) 
+#     X.append(vector) 
+# X = np.asarray(X)
 
-print(X)
+# print(X)
+
+'''
+that finds the most common ngrams that contain a particular target word.
+'''
+
+# fd = FreqDist(ng
+#               for ng in ngrams(text2, 5)
+#               if freq_words in ng)
+# for hit in fd.setdefault():
+#     print(' '.join(hit))
+
+
 
 tokenized_text = sent_tokenize(text)
 print("sent_tokenize", tokenized_text)
